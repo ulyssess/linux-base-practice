f446130ae387bdf2104dae30b498e71627b7bc38
virtual-ulyssess <aichaowang@localhost.none>
2017-05-21 18:28:17 +0800

    添加多线程spin锁例子，同时将sync调整为mutex例子

--- thread/mutex.c
+++ thread/mutex.c
@@ -1,77 +1,77 @@
+// 实现三个线程A B C，按照顺序循环输出 A B C
+
 #include <stdio.h>
 #include <pthread.h>
-#include <errno.h>
-#include <string.h>
 #include <unistd.h>
 
+int seq = 0;
 pthread_mutex_t mutex;
 
-void * thread1(void *arg)
+void *thread1(void *arg)
 {
-    printf("thread 1 try lock\n");
-    pthread_mutex_lock(&mutex);
-    printf("thread 1 locked\n");
-    printf("thread 1, wait 10s\n");
-    sleep(10);
-    pthread_mutex_unlock(&mutex);
+    int i;
 
-    return NULL;
-}
-
-void * thread2(void *arg)
-{
-    printf("thread 2 try lock\n");
-    pthread_mutex_lock(&mutex);
-    printf("thread 2 locked\n");
-    printf("thread 2\n");
-    pthread_mutex_unlock(&mutex);
-
-    return NULL;
-}
-
-void * thread3(void *arg)
-{
-    int ret;
-
-    printf("thread 3 try lock\n");
-    ret = pthread_mutex_trylock(&mutex);
-    if (ret != 0) {
-        //man里面说的设置errno是有问题的，errno没设置，倒是在返回值里面设置了.....
-        printf("ret = %d errno = %d , %s\n", ret, errno, strerror(errno));
-        if (ret == EBUSY) {
-            printf("Mutex is already locked.\n");
-            return NULL;
-        } else if (ret == EINVAL) {
-            printf("The value specified by mutex is invalid.\n");
-        } else {
-            printf("try lock error\n");
+    for (i = 0; i < 10; ) {
+        pthread_mutex_lock(&mutex);
+        if (seq == 0) {
+            printf("A\n");
+            i++;
+            seq = 1;
         }
+        pthread_mutex_unlock(&mutex);
     }
 
-    printf("thread 3 locked\n");
-    printf("thread 3\n");
-    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+void *thread2(void *arg)
+{
+    int i;
+
+    for (i = 0; i < 10; ) {
+        pthread_mutex_lock(&mutex);
+        if (seq == 1) {
+            printf("B\n");
+            i++;
+            seq = 2;
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+    return NULL;
+}
+
+void *thread3(void *arg)
+{
+    int i;
+
+    for (i = 0; i < 10; ) {
+        pthread_mutex_lock(&mutex);
+        if (seq == 2) {
+            printf("C\n");
+            i++;
+            seq = 0;
+        }
+        pthread_mutex_unlock(&mutex);
+    }
 
     return NULL;
 }
 
 int main()
 {
-    printf("Test thread mutex\n");
-
+    
     pthread_t tpid1;
     pthread_t tpid2;
     pthread_t tpid3;
 
-    if (pthread_mutex_init(&mutex, NULL) != 0) {
-        fprintf(stderr, "mutex init error: %s\n", strerror(errno));
-    }
+    pthread_mutex_init(&mutex, NULL);
 
     pthread_create(&tpid1, NULL, thread1, NULL);
     pthread_create(&tpid2, NULL, thread2, NULL);
     pthread_create(&tpid3, NULL, thread3, NULL);
 
-    sleep(50);
-
+    sleep(10);
+    
     return 0;
 }
--- thread/spin.c
+++ thread/spin.c
@@ -5,20 +5,20 @@
 #include <unistd.h>
 
 int seq = 0;
-pthread_mutex_t mutex;
+pthread_spinlock_t spin;
 
 void *thread1(void *arg)
 {
     int i;
 
     for (i = 0; i < 10; ) {
-        pthread_mutex_lock(&mutex);
+        pthread_spin_lock(&spin);
         if (seq == 0) {
             printf("A\n");
             i++;
             seq = 1;
         }
-        pthread_mutex_unlock(&mutex);
+        pthread_spin_unlock(&spin);
     }
 
     return NULL;
@@ -29,13 +29,13 @@ void *thread2(void *arg)
     int i;
 
     for (i = 0; i < 10; ) {
-        pthread_mutex_lock(&mutex);
+        pthread_spin_lock(&spin);
         if (seq == 1) {
             printf("B\n");
             i++;
             seq = 2;
         }
-        pthread_mutex_unlock(&mutex);
+        pthread_spin_unlock(&spin);
     }
 
     return NULL;
@@ -46,13 +46,13 @@ void *thread3(void *arg)
     int i;
 
     for (i = 0; i < 10; ) {
-        pthread_mutex_lock(&mutex);
+        pthread_spin_lock(&spin);
         if (seq == 2) {
             printf("C\n");
             i++;
             seq = 0;
         }
-        pthread_mutex_unlock(&mutex);
+        pthread_spin_unlock(&spin);
     }
 
     return NULL;
@@ -65,13 +65,15 @@ int main()
     pthread_t tpid2;
     pthread_t tpid3;
 
-    pthread_mutex_init(&mutex, NULL);
+    pthread_spin_init(&spin, PTHREAD_PROCESS_SHARED);
 
     pthread_create(&tpid1, NULL, thread1, NULL);
     pthread_create(&tpid2, NULL, thread2, NULL);
     pthread_create(&tpid3, NULL, thread3, NULL);
 
-    sleep(10);
+    pthread_join(tpid1, NULL);
+    pthread_join(tpid2, NULL);
+    pthread_join(tpid3, NULL);
     
     return 0;
 }
